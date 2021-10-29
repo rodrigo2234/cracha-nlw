1ed1e61bd9b651c368e6bb775095c805a9e312e5
rodrigo kalck <91804337+rodrigo2234@users.noreply.github.com>
2021-10-29 23:12:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'rodrigo2234',
+  youtube: 'rocketseat',
+  facebook: 'rodrigokalck58',
+  instagram: 'rodrigo_ofc.18',
+  twitter: 'rocketseat'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
